a0a63d8c9e085ee0c6e84948d62f0a65242f936b
2310532573 <lq2310532573@outlook.com>
2021-04-08 09:26:53 +0400

    修复无法读取Mapper xml错误

--- src/main/java/com/luolin/mapper/ExperienceMapper.java
+++ src/main/java/com/luolin/mapper/ExperienceMapper.java
@@ -8,18 +8,18 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface ExperienceMapper {
 
-	public int create(Experience experience);
+    int create(Experience experience);
 
-	public int delete(Integer id);
+    int delete(Integer id);
 
-	public int update(Experience experience);
+    int update(Experience experience);
 
-	public int updateSelective(Experience experience);
+    int updateSelective(Experience experience);
 
-	public List<Experience> query(Experience experience);
+    List<Experience> query(Experience experience);
 
-	public Experience detail(Integer id);
+    Experience detail(Integer id);
 
-	public int count(Experience experience);
+    int count(Experience experience);
 
 }
